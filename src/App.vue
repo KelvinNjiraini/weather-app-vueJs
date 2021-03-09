bc8c0d3a9bd60d3c4528522cf51766478bda453a
Kelvin Njiraini <njirainikelvin@gmail.com>
2021-03-09 21:16:37 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    
+  <div class="flex flex-col items-center py-10 px20 bg-yellow-600 h-screen">
+    <form class=" pb-10">
+      <input type="text" placeholder="Type in the city here..." class=" text-lg focus:outline-none rounded py-2 px-4">
+    </form>
+    <div class=" h-96 w-72 bg-green-500 rounded">
+      
+    </div>
   </div>
 </template>
 
