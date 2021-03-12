a9c27dcb0658b4402da6da48b12775440463038a
Kelvin Njiraini <njirainikelvin@gmail.com>
2021-03-12 14:25:42 +0300

    final touches

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="flex flex-col items-center py-10 px20 bg-yellow-600 h-screen">
+  <div class="flex flex-col items-center py-10 px20 h-screen" :class="this.weatherDetails.isDay ?'day': 'night'">
+    <h1 class=" text-4xl text-gray-800 pb-6">Weather in</h1>
     <form class=" pb-10" v-on:submit.prevent="getCity">
-      <input type="text" placeholder="Type in the city here..." class=" text-lg focus:outline-none rounded-lg py-2 px-4" v-model="citySearch">
+      <input type="text" placeholder="Type in the city here..." class=" text-lg focus:outline-none rounded-lg py-2 px-4 border border-gray-400 border-solid" v-model="citySearch">
     </form>
-    <div class=" h-96 w-72 bg-green-500 rounded-lg flex flex-col items-center justify-around">
+    <div class=" h-96 w-72 bg-gray-400 rounded-lg flex flex-col items-center justify-around shadow-2xl" v-if="visible">
       <p class=" text-xl">{{ this.weatherDetails.cityName }}</p>
       <p class=" text-3xl">&hellip;</p>
       <p class="text-xl">{{ this.weatherDetails.country }}</p>
-      <span class="text-4xl">{{ this.weatherDetails.temperature }}<sup>o</sup></span>
+      <span class="text-4xl">{{ this.weatherDetails.temperature }}<sup>o</sup><span class=" uppercase">C</span></span>
       <p>cloud like</p>
       <div class="flex justify-between">
         <div class=" mr-6">
@@ -24,14 +25,14 @@
 </template>
 
 <script>
-// import weatherStackapi from './api/weatherStackapi';
 import axios from 'axios';
 export default {
   name: 'App',
   data() {
     return{
       citySearch: "",
-      weatherDetails: {}    
+      weatherDetails: {},
+      visible: false    
     }
   },
   methods: {
@@ -52,6 +53,17 @@ export default {
       this.weatherDetails.temperature = response.data.current.temperature
       this.weatherDetails.feelslike = response.data.current.feelslike
       this.weatherDetails.humidity = response.data.current.humidity
+      this.weatherDetails.isDay = true
+
+      const timeOfDay = response.data.current.is_day;
+
+      if(timeOfDay.includes('n')) {
+        this.weatherDetails.isDay = false 
+      } else {
+        this.weatherDetails.isDay = true
+      }
+
+      this.visible = true;
     }
   }
 }
